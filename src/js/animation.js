8a9d129b7066f6d9edc5195bab538547336b306a
AVMaslennikov <AVMaslennikov@inno.tech>
2023-10-04 08:46:28 +0300

    test 7

--- src/js/animation.js
+++ src/js/animation.js
@@ -9,7 +9,7 @@ const modal = document.querySelector('.modal__wrapper')
 const test = document.querySelector('.test')
 
 const stats = new Stats();
-stats.showPanel(1); // 0: fps, 1: ms, 2: mb, 3+: custom
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
 test.appendChild(stats.dom);
 
 import {data} from './data.js'
